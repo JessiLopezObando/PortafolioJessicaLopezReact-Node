276474ec64534a868025f759cc277373310d5337
JessiLopezObando <jessicaandrealopezobando@gmail.com>
2023-06-17 17:43:24 -0500

    About me

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,9 +1,45 @@
 import React from 'react'
 import './about.css'
+import ME from '../../assets/me-about.jpg'
+import {FaAward} from 'react-icons/fa'
+import {VscFolderLibrary} from 'react-icons/vsc'
 
 const About = () => {
   return (
-    <section id='about'>About</section>
+    <section id='about'>
+      <h5>Get to know</h5>
+      <h2>About me</h2>
+    
+      <div className= "container about__container">
+        <div className="about__me">
+          <div className="about__me-image">
+            <img src={ME} alt="About Image" />
+          </div>
+        </div>
+
+        <div className="about__contenT">
+          <div className="about__cards">
+            <article className= 'about__card'> 
+              <FaAward className='about__icon'/>
+              <h5>Experience</h5>
+              <small>1- year working</small>
+            </article>
+
+            <article className= 'about__card'> 
+              <VscFolderLibrary className='about__icon'/>
+              <h5>Projects</h5>
+              <small>30+ Completed</small>
+            </article>
+          </div>
+          <p>
+            Lorem ipsum dolor sit amet consectetur ...
+          </p>
+          <a href="#contact" className='btn btn-primary'>Let's Talks</a>
+        </div>
+
+      </div>
+    </section>
+    
   )
 }
 
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -11,7 +11,7 @@ const Header = () => {
       <div className="container header__container">
         <h5> Hello I'm</h5>
         <h1> Jessica López</h1>
-        <h5 className="text-light">Front-end Developer</h5>
+        <h5 className="text-light">Testing Engineer | Front-end Developer</h5>
         <CTA />
         <HeaderSocial/>
 
